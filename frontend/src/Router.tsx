a6a462ac1dd8f9f1e91279fff985ed8f8126f6ec
David Santos <davidwallacedossantos@gmail.com>
2025-12-27 17:48:19 -0300

    feat: add layout react-router

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,8 +1,23 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Outlet } from "react-router";
+import Header from "./components/Header";
+
+const Layout = () => {
+  return (
+    <div className="flex min-h-screen flex-col bg-[#161410]">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello World</div>,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <div>Hello</div>,
+      },
+    ],
   },
 ]);
