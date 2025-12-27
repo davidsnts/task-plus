ebd0cc75eba3fc7da920feba137e32c9016c7158
David Santos <davidwallacedossantos@gmail.com>
2025-12-27 18:20:01 -0300

    feat:  Styling the header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,26 @@
+import { SquareUser, LogOut } from "lucide-react";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="bg-[#5A7ACD] h-12 flex">
+      <div className="max-w-4xl w-full flex items-center justify-between text-white mx-4 sm:mx-auto">
+        <div className="flex items-center">
+          <p className="font-bold text-2xl">TASK</p>
+          <span className="text-yellow-300 text-4xl font-bold w-12 h-12">
+            +
+          </span>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <SquareUser size={42} />
+          <p className="font-bold flex gap-2">
+            <span className="text-yellow-300">User</span>
+            <LogOut />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
